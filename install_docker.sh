59d56eb05d07f9831017736acda54871fb676840
Alex Ivanov <ivanov.alexey@onlyoffice.com>
2024-02-07 12:53:46 +0300

    Add Install Docker & Compose

---
+++ install_docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Installing dependencies";
+apt-get update && apt-get install -y apt-transport-https curl wget ca-certificates software-properties-common gnupg2 ntp net-tools
+echo "Installing Docker";
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update -y
+apt install -y docker-ce docker-ce-cli
+systemctl enable --now docker
+echo "Installing Docker Compose";
+VERSION_DOCKER_COMPOSE=$(curl -s https://api.github.com/repos/docker/compose/releases | jq -r '.[] | select(.prerelease==false) | .name' | sort -n | tail -n1);
+curl -L "https://github.com/docker/compose/releases/download/$VERSION_DOCKER_COMPOSE/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
