2884479b5d3ffa07d5b966966e7f87b099aba56f
Alex Ivanov <ivanov.alexey@onlyoffice.com>
2024-02-12 16:21:04 +0300

    Fixed the creation of certificates

--- install.sh
+++ install.sh
@@ -88,20 +88,28 @@ fi
 docker version | head -n2;
 docker-compose version | head -n1;
 
-if [ "$DOMAIN_NAME" != "" ]; then
-	if [ ! -f "/etc/letsencrypt/live/${DOMAIN_NAME}/fullchain.pem" ] && [ "$HTTP_PROTO" != "http" ]; then
-		echo -e "\nCertificate was not received\n"
 
-		bash nginx/nginx_le_cert_gen.sh ${PARAMETERS}
+if [ "$HTTP_PROTO" != "http" ]; then
 
+	if [ "$DOMAIN_NAME" != "" ]; then
+		if [ ! -f "/etc/letsencrypt/live/${DOMAIN_NAME}/fullchain.pem" ]; then
+			echo -e "\nCertificate was not received\n"
+
+			bash nginx/nginx_le_cert_gen.sh ${PARAMETERS}
+
+		else
+			echo -e "\nCertificates already exist\n";
+
+			mkdir -p /app/nginx/ssl/
+			cp -f /etc/letsencrypt/live/${DOMAIN_NAME}/fullchain.pem /app/nginx/ssl/
+			cp -f /etc/letsencrypt/live/${DOMAIN_NAME}/privkey.pem /app/nginx/ssl/
+		fi	
 	else
-		echo -e "\nCertificates already exist\n";
-		
-		mkdir -p /app/nginx/ssl/
-		cp -f /etc/letsencrypt/live/${DOMAIN_NAME}/fullchain.pem /app/nginx/ssl/
-		cp -f /etc/letsencrypt/live/${DOMAIN_NAME}/privkey.pem /app/nginx/ssl/
+		echo "To use the https protocol, you must specify the domain name!";
+		exit 1;
 	fi
 fi
+
 	case $INSTALLED_APP in
 		humhub )
 			source humhub/install_humhub_compose.sh 
