9b46b9a706c85d2d056a7115cb56260368a99b0d
Alex Ivanov <ivanov.alexey@onlyoffice.com>
2024-02-16 18:16:33 +0300

    Update purge.sh

--- purge.sh
+++ purge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#docker rm -f $(docker ps -qa)
-#docker volume rm $(docker volume ls -q)
+docker rm -f $(docker ps -qa)
+docker volume rm $(docker volume ls -q)
 #docker network rm $(docker network ls -q)
 docker system prune -af
 cd /
